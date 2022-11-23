d5225b17a79e15ac5de155fd9a76c8acaf87300f
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-23 15:32:34 -0300

    Header inicial

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,18 @@ import './styles.css';
 
 import { Link } from 'react-router-dom';
 
+import { FaSearch } from 'react-icons/fa';
+
 export default function Header() {
     return (
         <header>
-            
+            <img src="src/assets/watchflix-logo.svg" alt="Watchflix logo" />
+            <form className="search-bar">
+                <input type="text" placeholder='Faça a sua busca' />
+                <button>
+                    <FaSearch />
+                </button>
+            </form>
         </header>
     )
 }
