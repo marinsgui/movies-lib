af4557745902da7a1416e38391eb60c2913657df
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-26 16:06:16 -0300

    componente de loading criado

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,9 @@
+import loading from '../../assets/loading.svg';
+
+export default function Loading() {
+    return (
+        <main>
+            <img src={loading} alt="loading" />
+        </main>
+    )
+}
