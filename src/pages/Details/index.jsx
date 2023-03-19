7603ca8f618d7c19f1a408879422a56b5bf0674a
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-19 11:29:18 -0300

    pagina de detalhes alterada

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 
 import style from '../../styles/Details.module.css';
 
-
 export default function Details() {
     const moviesURL = import.meta.env.VITE_API;
     const apiKey = import.meta.env.VITE_API_KEY;
@@ -17,7 +16,6 @@ export default function Details() {
         const data = await res.json()
 
         setMovie(data)
-        console.log(data)
     }
 
     useEffect(() => {
@@ -26,23 +24,22 @@ export default function Details() {
     }, [])
     
     return (
-        <main className={style.container}>
+        <main className={style.movie_details}>
             <section>
-                <div className={style.movie_info}>
-                    <div className={style.infos}>
-                        <h2>{movie.title}</h2>
-                        <p><span>Título original:</span> {movie.original_title}</p>
-                        <p><span>Duração:</span> {movie.runtime} min.</p>
-                    </div>
-                    <div className={style.rating}>
-                        <p>&#x2B50; {movie.vote_average}</p>
-                    </div>
+                <div className={style.movie_header}>
+                    <h2>{movie.title}</h2>
+                    <p><span>Título original:</span> {movie.original_title}</p>
                 </div>
-                <div className={style.details_container}>
-                    <img src={imageURL + movie.poster_path} alt={movie.title} />
-                    <div className={style.overview}>
-                        <p><span>Sinopse:</span> {movie.overview}</p>
+                <div className={style.movie_info}>
+                    <div>
+                        <img src={imageURL + movie.poster_path} alt={movie.title} />
+                        <h3>&#x2B50; {movie.vote_average}</h3>
+                        <p><span>Duração:</span> {movie.runtime} min.</p>
+                        <p>{movie?.genres?.[0].name}</p>
+                        <p>{movie?.genres?.[1].name}</p>
+                        <p>{movie?.genres?.[2].name}</p>
                     </div>
+                    <p>{movie.overview}</p>
                 </div>
             </section>
         </main>
