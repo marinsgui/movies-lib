380e2dc3a654a23cedd7805590de389be804990d
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-21 14:26:18 -0300

    responsividade

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,10 @@
-import './styles.css';
+import style from '../../styles/Loading.module.css';
 
 import loading from '../../assets/loading.svg';
 
 export default function Loading() {
     return (
-        <div className='loading'>
+        <div className={style.loading}>
             <img src={loading} alt="loading" />
         </div>
     )
