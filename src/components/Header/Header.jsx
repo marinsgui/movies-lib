4182639053bb67f05bc968a44c9fb49a7a0c2bd9
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-23 11:08:12 -0300

    responsividade

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,24 +21,24 @@ export default function Header() {
     }
 
     return (
-        <header className='sticky top-0 z-10 bg-purple-800 flex justify-around items-center shadow-md'>
+        <header className='w-full md:sticky top-0 z-10 bg-purple-800 flex flex-col md:flex-row justify-around items-center shadow-md p-2 md:p-0'>
             <NavLink to='/'>
-                <img src={logo} alt="MoviesLib logo" className='w-40' />
+                <img src={logo} alt="MoviesLib logo" className='w-32 md:w-40' />
             </NavLink>
             
             <nav>
-                <ul className='list-none flex justify-between items-center gap-8'>
+                <ul className='list-none flex justify-between items-center p-4 gap-2 md:gap-8'>
                     <li>
-                        <NavLink to='/popular' className='font-bold text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Filmes populares</NavLink>
+                        <NavLink to='/popular' className='font-bold text-xs md:text-base text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Filmes populares</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/topratedmovies' className='font-bold text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Melhores filmes</NavLink>
+                        <NavLink to='/topratedmovies' className='font-bold text-xs md:text-base text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Melhores filmes</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/upcoming' className='font-bold text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Próximos lançamentos</NavLink>
+                        <NavLink to='/upcoming' className='font-bold text-xs md:text-base text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Próximos lançamentos</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/nowplaying' className='font-bold text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Filmes em cartaz</NavLink>
+                        <NavLink to='/nowplaying' className='font-bold text-xs md:text-base text-white hover:text-gray-300 active:border-b-2 border-gray-300'>Filmes em cartaz</NavLink>
                     </li>
                 </ul>
             </nav>
@@ -49,7 +49,7 @@ export default function Header() {
                 placeholder='Faça a sua busca'
                 onChange={(e) => setSearch(e.target.value)}
                 value={search}
-                className='w-80 h-8 p-3 border-b-2 border-gray-200 text-lg bg-purple-900 outline-none text-white focus:border-white placeholder:text-gray-300'
+                className='w-full md:w-80 h-8 p-3 border-b-2 border-gray-200 text-lg bg-purple-900 outline-none text-white focus:border-white placeholder:text-gray-300'
                 />
                 <button className='h-8 w-12 bg-transparent border-none cursor-pointer'>
                     <FaSearch className='w-2/3 h-2/3 text-white' />
