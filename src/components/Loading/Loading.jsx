50d33ff4d6f6f9cfa8710fe9ea060e60e584d391
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-22 23:34:31 -0300

    tailwind nos cards

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,8 @@
-import style from '../../styles/Loading.module.css';
-
 import loading from '../../assets/loading.svg';
 
 export default function Loading() {
     return (
-        <div className={style.loading}>
+        <div className='h-screen flex flex-col justify-center items-center'>
             <img src={loading} alt="loading" />
         </div>
     )
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,3 @@
-import style from '../../styles/MovieCard.module.css';
-
 import { Link } from 'react-router-dom';
 
 export default function MovieCard({ movie }) {
@@ -7,17 +5,17 @@ export default function MovieCard({ movie }) {
     const imageURL = import.meta.env.VITE_IMG;
 
     return (
-        <li className={style.card_container}>
-            <div className={style.card_img}>
-                <img src={imageURL + movie.poster_path} alt={movie.title} />
+        <li className='w-72 h-96 rounded-2xl relative flex items-end duration-500 shadow-lg before:content-[""] before:absolute before:top-0 before:left-0 before:block before:w-full before:h-full before:z-10 before:duration-500 before:rounded-md hover:translate-y-5 hover:before:opacity-100 hover:before:bg-[#00000096] group'>
+            <div className='w-full h-full absolute top-0 left-0'>
+                <img src={imageURL + movie.poster_path} alt={movie.title} className='w-full h-full rounded-2xl' />
             </div>
-            <div className={style.card_info}>
-                <h2 className={style.card_title}>{movie.title}</h2>
-                <p className={style.text_title}><span>Data de lançamento:</span> {movie.release_date}</p>
-                <p className={style.text_body}>&#x2B50; {movie.vote_average}</p>
+            <div className='relative z-30 p-4 pb-7 text-gray-200 opacity-0 translate-y-10 duration-500 group-hover:opacity-100 group-hover:translate-y-1'>
+                <h2>{movie.title}</h2>
+                <p className='text-base font-medium'>Data de lançamento: {movie.release_date}</p>
+                <p className='tracking-wide text-sm mt-1 mb-4'>&#x2B50; {movie.vote_average}</p>
                 <Link 
                     to={`/details/${movie.id}`} 
-                    className={style.card_button}
+                    className='p-2 outline-none border-none rounded bg-purple-800 font-bold text-white hover:bg-purple-900 hover:text-gray-200'
                 >
                     Veja mais
                 </Link>
