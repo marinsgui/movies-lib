927b69d75357b6f0b2c739bc632c2120d340ff56
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-19 16:37:11 -0300

    novo layout da página de detalhes

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { AiOutlineClockCircle } from "react-icons/ai";
+
 import Loading from "../../components/Loading/Loading";
 
 export default function Details() {
@@ -27,46 +29,66 @@ export default function Details() {
   }, []);
 
   return (
-    <main className="min-h-[90vh] md:py-8 px-4 bg-slate-100 dark:bg-gray-800">
+    <main
+      className="min-h-[90vh] md:py-8 px-4 bg-slate-100 dark:bg-gray-800"
+      style={{
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0), rgba(0, 0, 0, 0.5)), url(${
+          imageURL + movie.backdrop_path
+        })`,
+        backgroundPosition: "center right",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       {loading && <Loading />}
 
       {movie && (
-        <section className="max-w-4xl mx-auto text-black dark:text-white">
-          <div className="pt-5 md:flex md:flex-col md:justify-between md:items-center md:mb-5">
-            <h2 className="m-0 text-4xl">{movie.title}</h2>
+        <section className="flex flex-col justify-end max-w-4xl h-[80vh] text-white">
+          <div className="md:flex md:flex-col md:items-center">
+            <h2 className="text-6xl text-center my-5">{movie.title}</h2>
             <p>
               <span className="font-bold">Título original:</span>{" "}
               {movie.original_title}
             </p>
           </div>
-          <div className="py-5 md:flex md:justify-around md:items-center md:gap-4">
+          <div className="md:flex md:justify-around md:items-end md:gap-4">
             <div className="flex flex-col items-start md:min-w-[30%]">
               <img
                 src={imageURL + movie.poster_path}
                 alt={movie.title}
                 className="rounded-2xl"
               />
-              <h3>&#x2B50; {movie.vote_average}</h3>
-              <p>
-                <span className="font-bold">Duração:</span> {movie.runtime} min.
-              </p>
-              {movie.genres && (
-                <p className="bg-purple-800 rounded-2xl p-2 my-2 text-white">
-                  {movie.genres[0].name}
-                </p>
-              )}
-              {movie.genres?.[1] && (
-                <p className="bg-purple-800 rounded-2xl p-2 my-2 text-white">
-                  {movie.genres[1].name}
-                </p>
-              )}
-              {movie.genres?.[2] && (
-                <p className="bg-purple-800 rounded-2xl p-2 my-2 text-white">
-                  {movie.genres[2].name}
-                </p>
-              )}
             </div>
-            <p className="md:-mt-40 text-xl">{movie.overview}</p>
+            <div className="flex flex-col justify-around items-start">
+              <div className="flex items-center my-5">
+                <div className="mr-5">
+                  <h3>&#x2B50; {movie.vote_average}</h3>
+                  <p>
+                    <AiOutlineClockCircle
+                      size={20}
+                      style={{ display: "inline" }}
+                    />{" "}
+                    {movie.runtime} min.
+                  </p>
+                </div>
+                {movie.genres && (
+                  <p className="bg-purple-800 rounded-2xl p-2 mx-2 text-white">
+                    {movie.genres[0].name}
+                  </p>
+                )}
+                {movie.genres?.[1] && (
+                  <p className="bg-purple-800 rounded-2xl p-2 mx-2 text-white">
+                    {movie.genres[1].name}
+                  </p>
+                )}
+                {movie.genres?.[2] && (
+                  <p className="bg-purple-800 rounded-2xl p-2 mx-2 text-white">
+                    {movie.genres[2].name}
+                  </p>
+                )}
+              </div>
+              <p className="text-xl">{movie.overview}</p>
+            </div>
           </div>
         </section>
       )}
