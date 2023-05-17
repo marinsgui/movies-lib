896f2cf4b607650562855da273eedcb11acd148b
marinsgui <marins0guilherme@gmail.com>
2023-05-17 11:56:21 -0300

    alteração no tamanho da homepage paga mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import image from "../assets/home-cinema-animate.svg";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen gap-5 p-4 bg-slate-100 dark:bg-gray-800">
+    <main className="flex justify-center items-center h-[77vh] gap-5 p-4 bg-slate-100 dark:bg-gray-800">
       <img src={image} className="md:w-1/2" />
     </main>
   );
